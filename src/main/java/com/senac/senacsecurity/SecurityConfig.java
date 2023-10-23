9121223ec0bd2705a7b6c5c63599ff0dd1941f23
Alysson Oliveira <3oalysson@gmail.com>
2023-10-23 13:54:39 -0300

    Update SecurityConfig.java

--- src/main/java/com/senac/senacsecurity/SecurityConfig.java
+++ src/main/java/com/senac/senacsecurity/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/login?logout")
                 .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID"); 
+                .deleteCookies("JSESSIONID");
     }
 }
 
